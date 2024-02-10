8fcc4e57d6e9b53d353714c5320721e1cd25af57
Hazmed <mail@hazmedmoreno.com>
2024-02-09 19:19:35 -0500

    add: favicon | upd: demo overlay

--- src/app/shared/demo.js
+++ src/app/shared/demo.js
@@ -1,5 +1,5 @@
 "use client";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faInfoCircle, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
@@ -11,28 +11,32 @@ export default function DemoOverlay() {
   };
 
   return open ? (
-    <div className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-50 z-50 flex justify-center items-center">
-      <div className="bg-white px-10 rounded-md shadow-lg relative max-w-lg py-20 mx-4">
+    <div className="fixed top-0 left-0 h-screen w-screen bg-black bg-opacity-50 z-50 flex justify-center items-center text-white">
+      <div className="bg-zinc-900 shadow-blue-600 px-10 rounded-md shadow-lg relative max-w-lg py-20 mx-4 z-10">
         <button
           onClick={toggleOpen}
           className="absolute top-0 right-0 m-4 z-10"
           aria-label="close demo modal"
         >
-          <FontAwesomeIcon icon={faTimes} className="text-black fa-lg" />
+          <FontAwesomeIcon icon={faTimes} className="fa-lg" />
         </button>
         <h2 className="text-3xl font-bold mb-2">Demo</h2>
         <p className="text-lg font-light">
           This site is a demo site built by Full Summit. All content is fictional. For demonstration purposes only.
         </p>
       </div>
+      <div className="absolute w-full z-0 bg-transparent h-full" onClick={toggleOpen}></div>
     </div>
   ) : (
     <div
       onClick={toggleOpen}
       aria-label="Toggle Demo Modal"
-      className="hover:cursor-pointer fixed right-0 top-[50%] rounded-l-full shadow-lg shadow-zinc-400 bg-blue-600 font-semibold border-none text-white pl-6 pr-3 py-4 z-50 hover:bg-blue-700 hover:shadow-zinc-500 transition-colors"
+      className="hover:cursor-pointer hover:pr-6 group transition-all fixed right-0 top-[50%] rounded-l-full shadow-lg bg-zinc-950 font-semibold shadow-blue-600 border-none text-white pl-6 pr-3 py-3 z-50 hover:bg-zinc-800"
     >
-      <span className="text-lg">demo</span>
+      <div className="flex items-center justify-center gap-2">
+        <span className="text-sm text-blue">demo</span>
+        <FontAwesomeIcon icon={faInfoCircle} className="hidden group-hover:flex" size="xs"/>
+      </div>
     </div>
   );
 }
